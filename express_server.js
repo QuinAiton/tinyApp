946e9200db4cc4247f03f728875442ec3c022251
QuintenAiton <quinaiton@hotmail.com>
2021-01-18 21:25:10 +0000

    added get route to urls database

--- express_server.js
+++ express_server.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
   res.send('hello');
 });
 
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase)
+});
+
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`)
 });
