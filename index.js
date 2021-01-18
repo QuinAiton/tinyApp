35bf6284077eaad9f7c2194f7141e71f9f4c6fef
QuintenAiton <quinaiton@hotmail.com>
2021-01-18 21:31:35 +0000

    added hello path to routes, changes main file name to index.js for nodemon

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express'),
+  app = express(),
+  PORT = 8080;
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+}
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase)
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`)
+});
