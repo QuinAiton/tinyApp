092fa8945372d1ac3be81c3ac83eb5bf8695e0a5
QuintenAiton <quinaiton@hotmail.com>
2021-01-19 02:27:25 +0000

    added generaterandomString function and added a post request to handle new urls

--- express_server.js
+++ express_server.js
@@ -1,19 +1,57 @@
 const express = require('express'),
+  bodyParser = require('body-parser'),
   app = express(),
   PORT = 8080;
 
+
+//environment set up
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//database
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 }
 
+//Routes
 app.get('/', (req, res) => {
   res.send('hello');
 });
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase)
+})
+
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
 });
+
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new')
+})
+
+app.post('/urls', (req, res) => {
+  console.log(req.body)
+  res.send()
+});
+
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL }
+  res.render('urls_show', templateVars)
+})
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+const generateRandomString = () => {
+  let randomString = Math.random().toString(36).slice(2)
+  return randomString.slice(1, 7)
+}
+
 
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`)
