7bc815fcb48a81573af50103af3ff627aa98699a
QuintenAiton <quinaiton@hotmail.com>
2021-01-19 23:56:25 +0000

    added username cookies to templateVars to be used on all pages

--- express_server.js
+++ express_server.js
@@ -34,7 +34,8 @@ app.get('/urls', (req, res) => {
 
 //create routes
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  templateVars = { username: req.cookies.username }
+  res.render('urls_new', templateVars);
 });
 
 app.post('/urls', (req, res) => {
