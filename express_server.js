4f9b3773ab07b9504919c8381d3b4b84f905d40a
QuintenAiton <quinaiton@hotmail.com>
2021-01-20 22:00:48 +0000

    added registration routes to handle new user signups

--- express_server.js
+++ express_server.js
@@ -4,16 +4,16 @@ const express = require('express'),
   cookieParser = require('cookie-parser'),
   app = express(),
   PORT = 8080;
-//-----------------------------------------------------------------------------------------------
 
 
-//environment set up
+
+//----------------------------environment set up
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-//-----------------------------------------------------------------------------------------------
+//
 
 
 //database
@@ -21,22 +21,27 @@ let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-//-----------------------------------------------------------------------------------------------
 
+let users = {
 
-//Routes
+}
+
+//
+
+//-------------------------------------Routes
+
 app.get('/', (req, res) => {
   res.send('hello');
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies.username };
+  const templateVars = { urls: urlDatabase, user: users[req.cookies.user_id] };
   res.render('urls_index', templateVars);
 });
 
 //create routes
 app.get('/urls/new', (req, res) => {
-  templateVars = { username: req.cookies.username }
+  templateVars = { user: users[req.cookies.user_id] }
   res.render('urls_new', templateVars);
 });
 
@@ -48,7 +53,7 @@ app.post('/urls', (req, res) => {
 
 //show route
 app.get('/urls/:id', (req, res) => {
-  const templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies.username };
+  const templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id], user: users[req.cookies.user_id] };
   res.render('urls_show', templateVars);
 });
 
@@ -60,7 +65,7 @@ app.post('/urls/:id/delete', (req, res) => {
 
 //update routes
 app.get('/urls/:id/update', (req, res) => {
-  const templateVars = { name: req.params.id, username: req.cookies.username };
+  const templateVars = { name: req.params.id, user: users[req.cookies.user_id] };
   res.render('urls_update', templateVars);
 });
 
@@ -70,7 +75,27 @@ app.post('/urls/:id', (req, res) => {
   res.redirect('/urls/' + shortURL);
 });
 
-//Authentification routes
+//----------------------------Authentification
+
+//registration routes
+app.get('/registration', (req, res) => {
+  templateVars = { user: users[req.cookies.user_id] }
+  res.render('userRegistration', templateVars)
+})
+
+app.post('/registration', (req, res) => {
+  let uniqueId = generateRandomString();
+  users[uniqueId] = {
+    id: uniqueId,
+    email: req.body.user.username,
+    password: req.body.user.password,
+  }
+  res.cookie('user_id', uniqueId)
+  console.log(`user generated ${JSON.stringify(users[req.cookies.user_id])}`)
+  res.redirect('/urls')
+})
+
+//login and logout routes
 app.post('/login', (req, res) => {
   res.cookie('username', req.body.username)
   res.redirect('/urls')
@@ -81,23 +106,21 @@ app.post('/logout', (req, res) => {
   res.redirect('/urls')
 })
 
-//redirect route
+//Long URl redirect route
 app.get('/u/:id', (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
 });
 
-//-----------------------------------------------------------------------------------------------
+//----------------------------Application functions
 
-//application functions
 const generateRandomString = () => {
   let randomString = Math.random().toString(36).slice(2);
   return randomString.slice(1, 7);
 };
-//-----------------------------------------------------------------------------------------------
+
 
 //server
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
 });
-//-----------------------------------------------------------------------------------------------
